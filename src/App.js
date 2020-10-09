98b7e4dadf74bec68c25f4ee884455a1c73e14ca
danibotcode <45088418+danibotcode@users.noreply.github.com>
2020-10-09 11:48:55 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
+import Widgets from './components/Widgets.js';
+import Login from './components/Login.js';
+import { useStateValue } from "./StateProvider";
+
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+
   return (
     // BEM naming convention
     <div className="app">
-      
-      <Header />
+
+    {!user ? (
+      <Login />)
+       : (
+         <>
+         <Header />
+         <div className="app__body">
+           <Sidebar />
+           <Feed />
+
+           {/* Feed */}
+           {/* Widgets */}
+           <Widgets />
+         </div>  
+        </>
+       )}
     
-
-    <div className="app__body">
-        <Sidebar />
-        <Feed />
-
-   
-       
-        {/* Feed */}
-        {/* Widgets */}   
-        </div>     
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyC00Xjz1x6ow5PlOirgIUTX-kgP5QkoTOw",
@@ -9,3 +11,9 @@ const firebaseConfig = {
     appId: "1:860518538143:web:e4fa0d013e1fd34ee53baa",
     measurementId: "G-7JKGZJEX94"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const db = firebaseApp.firestore();
+
+export const auth = firebase.auth();
+export const provider = new firebase.auth.GoogleAuthProvider();
